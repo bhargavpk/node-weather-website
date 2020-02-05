7ed0e7a6d2f517d74b231d7491643e2a2aac5dba
BhargavPK <bpk19900@gmail.com>
2020-02-05 18:57:47 +0530

    Text changes to help page

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.static(publicPath));
 app.get('',(req,res)=>{
     res.render('index',{
         title:'Weather App',
-        author:'Bhargav'
+        author:'Bhargav PK'
     });
 });
 
